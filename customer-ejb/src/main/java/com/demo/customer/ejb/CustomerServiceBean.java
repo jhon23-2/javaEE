db6d7ae0ee95237c269de2bec11669aaf791c4ae
jhonatanalmanza <jhoalmanza52@gmail.com>
2026-05-10 16:26:08 -0500

    Logs added to the CustomerServiceBean class

--- customer-ejb/src/main/java/com/demo/customer/ejb/CustomerServiceBean.java
+++ customer-ejb/src/main/java/com/demo/customer/ejb/CustomerServiceBean.java
@@ -3,13 +3,16 @@ package com.demo.customer.ejb;
 import com.demo.customer.util.dto.CustomerRequestDTO;
 import com.demo.customer.util.dto.CustomerResponseDTO;
 import com.demo.customer.util.model.CustomerModel;
+import jdk.jpackage.internal.Log;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 @Stateless
@@ -17,6 +20,8 @@ public class CustomerServiceBean implements CustomerService{
 
     @PersistenceContext(unitName = "customerPU")
     private EntityManager entityManager;
+
+    private final Logger log = Logger.getLogger(CustomerServiceBean.class.getName());
 
     @Override
     public CustomerResponseDTO saveCustomer(CustomerRequestDTO customerRequestDTO) {
@@ -29,6 +34,8 @@ public class CustomerServiceBean implements CustomerService{
         if (!model.isPresent()) return new CustomerResponseDTO();
 
         CustomerModel customerCreated = model.get();
+        LocalDateTime dateCreation = LocalDateTime.now();
+        log.info("-------- User saved successfully id: " + customerCreated.getId()  + " Date Creation: " +dateCreation.toLocalDate().toString());
         CustomerResponseDTO response = new CustomerResponseDTO();
 
         response.setId(String.valueOf(customerCreated.getId()));
@@ -43,6 +50,8 @@ public class CustomerServiceBean implements CustomerService{
         List<CustomerModel> models = entityManager
                 .createQuery("SELECT c FROM CustomerModel c", CustomerModel.class)
                 .getResultList();
+
+        log.info("All users Fetched: " + models.size());
 
         return models.stream()
                 .map(m -> {
