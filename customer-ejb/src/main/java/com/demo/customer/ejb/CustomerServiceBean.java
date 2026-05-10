cfacd49f0c83d91641b5dae16f29bba429adea19
jhonatanalmanza <jhoalmanza52@gmail.com>
2026-05-10 15:03:51 -0500

    CustomerServiceBean is already done th bussiness logic implementation has been created succesfully

--- customer-ejb/src/main/java/com/demo/customer/ejb/CustomerServiceBean.java
+++ customer-ejb/src/main/java/com/demo/customer/ejb/CustomerServiceBean.java
@@ -2,11 +2,15 @@ package com.demo.customer.ejb;
 
 import com.demo.customer.util.dto.CustomerRequestDTO;
 import com.demo.customer.util.dto.CustomerResponseDTO;
+import com.demo.customer.util.model.CustomerModel;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Stateless
 public class CustomerServiceBean implements CustomerService{
@@ -16,11 +20,38 @@ public class CustomerServiceBean implements CustomerService{
 
     @Override
     public CustomerResponseDTO saveCustomer(CustomerRequestDTO customerRequestDTO) {
-        return null;
+
+        Optional<CustomerModel> model = Optional.ofNullable(this.entityManager.merge(new CustomerModel(
+                customerRequestDTO.getName(),
+                customerRequestDTO.getLastName()))
+        );
+
+        if (!model.isPresent()) return new CustomerResponseDTO();
+
+        CustomerModel customerCreated = model.get();
+        CustomerResponseDTO response = new CustomerResponseDTO();
+
+        response.setId(String.valueOf(customerCreated.getId()));
+        response.setName(customerCreated.getName());
+        response.setLastName(customerCreated.getLastName());
+
+        return response;
     }
 
     @Override
     public Collection<CustomerResponseDTO> findAllCustomer() {
-        return null;
+        List<CustomerModel> models = entityManager
+                .createQuery("SELECT c FROM CustomerModel c", CustomerModel.class)
+                .getResultList();
+
+        return models.stream()
+                .map(m -> {
+                    CustomerResponseDTO dto = new CustomerResponseDTO();
+                    dto.setId(m.getId() == null ? null : String.valueOf(m.getId()));
+                    dto.setName(m.getName());
+                    dto.setLastName(m.getLastName());
+                    return dto;
+                })
+                .collect(Collectors.toList());
     }
 }
